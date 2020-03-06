2ebd51d91c75d24a3f66908ca7c51a02ed3782f8
s19022 <s19022@LAB204-02>
2020-03-06 13:46:12 +0100

    initialize

---
+++ task_1/task_1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_1
+{
+    public class Person
+    {
+        private string _firstName;
+      
+
+        public string FirstName  //property
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+
+        //auto property
+        public string LastName
+        {
+            get; set;
+        }
+    }
+}
---
+++ task_1/task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+
+            var httpClient = new HttpClient();
+            var url = "https://www.pja.edu.pl";
+
+
+
+            var response = await httpClient.GetAsync(url);
+
+            var content = response.Content;
+
+        
+
+            var regex = new Regex("[*]");
+
+
+
+            var matches = regex.Matches(content.ToString());
+
+            Console.WriteLine(matches.);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            } 
+           
+        }
+    }
+}
